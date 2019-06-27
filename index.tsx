e4c85092ed298af6a75b3a7a28ef0c73d20904db
Gurban Gurbanov <gurban.gurbanov@pashabank.az>
2019-06-27 15:50:27 +0400

    Initial commit

---
+++ index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PaymentsModule: React.FC = () => {
+    return <div className="container">
+        <div className="row">
+            <div className="col-sm" style={{ marginTop: "20px" }}>
+                Payments module
+            </div>
+        </div>
+    </div>
+};
+
+export default PaymentsModule;
